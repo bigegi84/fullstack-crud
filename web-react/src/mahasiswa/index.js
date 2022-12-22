a31513c98c68e16ed605ff8453a2176d629c336c
bigegi84 <gilangpw84@gmail.com>
2022-12-22 07:19:08 +0000

    up

--- web-react/src/mahasiswa/index.js
+++ web-react/src/mahasiswa/index.js
@@ -1,14 +1,34 @@
 import React, { useState, useEffect } from "react";
 export default () => {
   const [data, setData] = useState([1, 2, 3, 4, 5, 6, 7, 7]);
+  const [input, setInput] = useState(null);
   useEffect(() => {
     console.log("This will run whenever the App component renders!");
   }, []);
   useEffect(() => {
     console.log("This will also run whenever the App component renders!");
   }, []);
+  const onSimpan = () => {};
   return (
     <div>
+      <input
+        type={"text"}
+        value={input}
+        onChange={(e) => {
+          console.log("masuk onchange");
+          setInput(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          const newData = [...data];
+          newData.push(input);
+          setData(newData);
+          setInput(null);
+        }}
+      >
+        simpan
+      </button>
       {data.map((it) => {
         return (
           <tr>
